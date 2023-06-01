56087242e347d3a60e2ee1528834f13404105680
BesaMugi <mugiwarabesa@mail.ru>
2023-06-01 10:37:48 +0300

    убрал не работающее

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
+button.addEventListener("click", (e) => {
+  const list = document.getElementById("list");
+  const input = document.getElementById("input");
+//   const button = document.getElementById("button");
+  const input2 = document.createElement("input");
+  const p = document.createElement("p");
 
-button.addEventListener('click', (e) => {
-    const list = document.getElementById('list');
-    const input = document.getElementById('input');
-    const button = document.getElementById('button');
-    const input2 = document.createElement('input');
-    const p = document.createElement('p');
-    
-input2.type = 'checkbox'
+  input2.type = "checkbox";
 
-e.preventDefault();
+  e.preventDefault();
 
-const li = document.createElement('li');
+  const li = document.createElement("li");
 
-p.textContent = input.value;
+  p.textContent = input.value;
 
-const chekBox = document.createElement('input')
+//   const chekBox = document.createElement("input");
 
-const deleteButton = document.createElement('button');
-deleteButton.textContent = 'X';
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
 
-deleteButton.addEventListener('click', (e) =>{
+  deleteButton.addEventListener("click", (e) => {
     e.target.parentElement.remove();
-})
+  });
 
-input2.addEventListener('click', (e) => {
-    if(input2.checked === true){
-        p.style.textDecoration = "line-through"
+  input2.addEventListener("click", (e) => {
+    if (input2.checked === true) {
+      p.style.textDecoration = "line-through";
     }
-    if(input2.checked === false){
-        p.style.textDecoration = "none"
+    if (input2.checked === false) {
+      p.style.textDecoration = "none";
     }
-})
+  });
 
-if( input.value.trim() === ''){
-    return alert('ты че э')
-}
+  if (input.value.trim() === "") {
+    return alert("ты че э");
+  }
 
-li.append(p)
-li.prepend(input2);
-list.append(li);
-li.append(deleteButton);
+  li.append(p);
+  li.prepend(input2);
+  list.append(li);
+  li.append(deleteButton);
 
+  input.value = "";
 
-input.value = '';
-
-input2.style.width = '45px'
-deleteButton.id = 'buttonOne'
-input2.classList.add('input2')
+  input2.style.width = "45px";
+  deleteButton.id = "buttonOne";
+  input2.classList.add("input2");
 });
